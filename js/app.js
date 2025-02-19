d03941fe25c6030c503da0cbbe2a420c0cd72a96
= <syversonshea@gmail.com>
2025-02-19 12:52:43 -0700

    Header File, and Button Change.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const button = document.getElementById('theButton');
 button.addEventListener("click", function() {
     count += 1; 
     if(count == 1){
-        alert(`You have pressed the button 1 time. Thanks for visiting!`);
+        alert(`You have pressed the button 1 time. Have a nice day!`);
     }
     else if (count > 9){
         alert(`You have pressed the button ${count} times. That is too many.`);
