12b6e88290ed6ae042ae2c9ea2ae5f86b0f312eb
Shea Syve <syversonshea@gmail.com>
2026-03-30 22:25:17 -0600

    New Version with WebPack

---
+++ src/home_page/interests.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function InterestsSection() {
+    // 1. A simple array of strings
+    const myInterests = [
+        "Data Structures & Algorithms",
+        "Computer Graphics",
+        "Machine Learning",
+        "Simulations",
+        "Artificial Intelligence",
+        "Competitive Programming",
+        "Relational Databases",
+        "Computer Security"
+    ];
+
+    return (
+        <div className="list">
+            <h3>Interests</h3>
+            <ul className="bulletlist">
+                {myInterests.map((interest, index) => (
+                    <li key={index}>{interest}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default InterestsSection;
