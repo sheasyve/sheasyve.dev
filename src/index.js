462efa1250c0edc426fd51c2644ef457f9b2bac3
Shea Syve <syversonshea@gmail.com>
2026-03-30 22:37:46 -0600

    Fix .gitignore tracking again 2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './app.js'; 
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(<App />);
