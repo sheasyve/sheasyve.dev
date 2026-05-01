9fc86d8929c529411dc6b22af03a8e92f0700673
Shea Syve <syversonshea@gmail.com>
2026-04-30 20:31:33 -0600

    CORS

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const app = express();
 // Check if running on Window
 const isWindows = process.platform === 'win32';
 
-const dbPath = "localhost:/var/lib/firebird/data/visitor_counter.fdb";
+const dbPath = "/var/lib/firebird/data/visitor_counter.fdb";
 const auth = `-user sysdba -password '${process.env.DB_PASSWORD}'`;
 
 const runSql = (query) => {
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 git pull origin main
 set -e
 npm install --production
+npm install cors
 pm2 restart visitor-api
 sudo systemctl restart nginx
 
