a8246e5d964ea27c6924183f57989ba9e361ae3d
Shea Syve <syversonshea@gmail.com>
2026-04-30 21:13:28 -0600

    masking

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { exec } = require('child_process');
-require('dotenv').config();
+
+// The one fix we MUST keep so PM2 finds your password
+require('dotenv').config({ path: __dirname + '/.env' });
+
 const cors = require('cors'); 
 const app = express();
 
