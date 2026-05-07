a315756ce91e996c2b0981e4892c6de4fad8861b
Shea Syve <syversonshea@gmail.com>
2026-05-06 20:30:32 -0600

    Full Writeup of site done barring images

--- src/home_page/projects.js
+++ src/home_page/projects.js
@@ -7,7 +7,7 @@ function ProjectsSection() {
     const myProjects = [
         { url: "/devsite", name: "Portfolio Website - sheasyve.dev", tech: "– (JavaScript, React, HTML, CSS)" },
         { url: "/birdflow", name: "BirdFlow - Wind Simulator Extension for Blender", tech: "– (Cython, Python)" },
-        { url: "/ascii-rt", name: "AsciiRT - Real-time ray-traced animations in the terminal.", tech: "– (CUDA, C++)" },
+        { url: "/ascii-rt", name: "AsciiRT - Real-time Ray-traced Animations in the Terminal.", tech: "– (CUDA, C++)" },
         { url: "/super-ai-bros", name: "Super AI Bros - Reinforcement Learning AI for Super Mario Bros", tech: "– (Python)" },
         { url: "/teeny-timer", name: "TeenyTimer - Cross-Platform Countdown Timer", tech: "– (Python)" }
     ];
