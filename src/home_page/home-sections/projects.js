5827c9961dd488298ac701f4042a9a7138b71038
Shea Syve <syversonshea@gmail.com>
2026-06-11 21:51:05 -0600

    New Project

--- src/home_page/home-sections/projects.js
+++ src/home_page/home-sections/projects.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 function ProjectsSection() {
 
     const myProjects = [
+        { url: "/simplecrush", name: "SimpleCrush - Audio Distortion Plugin", tech: "– (C++, JUCE)" },
         { url: "/devsite", name: "Portfolio Website - sheasyve.dev", tech: "– (JavaScript, React, HTML, CSS)" },
         { url: "/birdflow", name: "BirdFlow - Wind Simulator Extension for Blender", tech: "– (Cython, Python)" },
         { url: "/ascii-rt", name: "AsciiRT - Real-time Ray-traced Animations in the Terminal.", tech: "– (CUDA, C++)" },
