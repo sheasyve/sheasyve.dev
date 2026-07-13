d18a9e73d3f1bc3fad3c81f9feb16a8cafe0cc8f
Shea Syve <syversonshea@gmail.com>
2026-07-12 18:51:49 -0600

    Fixed Download

--- src/project_pages/simplecrush.js
+++ src/project_pages/simplecrush.js
@@ -16,7 +16,7 @@ import { FiDownload } from "react-icons/fi";
 const DownloadButton = () => {
   return (
     <a 
-      href="/downloads/SimpleCrush.vst3.zip" 
+      href="downloads/SimpleCrush.vst3.zip" 
       download="SimpleCrush.vst3.zip"
       target="_blank" 
       rel="noopener noreferrer"
