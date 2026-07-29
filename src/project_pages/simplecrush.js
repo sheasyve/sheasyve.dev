632684c2d1c9a592ec31abba889a6a798316064e
Shea Syve <syversonshea@gmail.com>
2026-07-28 23:10:11 -0600

    New Simplecrush images and writing

--- src/project_pages/simplecrush.js
+++ src/project_pages/simplecrush.js
@@ -3,6 +3,7 @@ import LoadVideo from '../components/load-video.js';
 import pluginPic from '../../media/projects/simplecrush/plugin.png';
 import settingsPic from '../../media/projects/simplecrush/settings.png';
 import themePic from '../../media/projects/simplecrush/themes.png';
+import presetPic from '../../media/projects/simplecrush/presets.png';
 import t1 from '../../media/projects/simplecrush/t1.png';
 import t2 from '../../media/projects/simplecrush/t2.png';
 import t3 from '../../media/projects/simplecrush/t3.png';
@@ -45,13 +46,18 @@ function SimpleCrushProject() {
         "Sample Rate Reduction: The plugin allows users to lower the sample rate of the audio, resulting in aliasing artifacts that contribute to the bitcrushed effect.",
         "Mix Control: A mix control enables users to blend the processed (bitcrushed) signal with the original dry signal, allowing for subtle or extreme effects.",
         "Filter Controls: Built-in high-pass and low-pass filters allow users to remove unpleasant high and low frequencies that can arise from bitcrushing, providing more control over the final sound.",
-        "Themes: This is in development and nearly finished. I need to dial in colorschemes but they are implemented and save in a file as a persistent setting. "
+        "Themes: Seven unique color themes are available for users to customize the plugin's appearance, enhancing the visual experience and allowing for personal preference.",
+        "Resizable UI: The plugin's user interface can be resized, allowing users to adjust the layout to their preference and screen size",
+        "Settings Menu: A settings menu provides access to various configuration options, including theme selection and other plugin preferences.",
+        "Presets: A fully featured preset system is complete with save, delete, load, random knob, and folder selection."
     ];
 
     const experience = ["Learned how to create VST3 plugins using C++ and the JUCE framework.",
         "Developed a deeper understanding of digital audio processing concepts, including bit depth and sample rate reduction algorithms.",
         "Gained experience in designing and implementing user interfaces for audio plugins, ensuring an intuitive and visually appealing user experience.",
         "Iterated on audio processing algorithms to achieve the desired sound quality and performance, balancing the trade-offs between audio fidelity and computational efficiency.",
+        "Learned how to implement a preset system for audio plugins, allowing users to save and recall their preferred settings.",
+        "Created a beautiful custom user interface with a consistent look and feel across different themes, enhancing my eye for design and user experience.",
         "Implemented a fully custom plugin development framework which I can reuse to create more plugins with a consistent look and feel, and with a more efficient development process."
     ];
 
@@ -82,16 +88,16 @@ function SimpleCrushProject() {
                     <DownloadButton />
                     <img src={pluginPic} alt="SimpleCrush Plugin" className="plugin-pic" style={{ margin: '1rem auto', display: 'block' }} />
                     The plugin allows users to reduce the bit depth and sample rate of audio, with built in mix and filter controls.
-                    This plugin is still in development, but is fully functional with a recent v1.1 release. This writeup is a work in progress and will be updated with more technical details and experience gained as development continues.
+                    There is also a settings menu with 7 unique color themes, a resizing tool, and a preset system with a randomizer.
+                    This writeup is a work in progress and will be updated with more technical details and experience gained as development continues.
                     <br></br><br></br>
-                    Currently I am finishing up the settings menu and color themes, as well as refining the user interface for a more polished experience.
-                    After that, I will add a few more audio processing features and then work on implementing presets.
+                    Currently I am finishing up the presets menu, and tuning the UI a bit more. 
+                    After that, I will be bundling an installer and adding hover hints that can be toggled in settings.
                 </h3>
 
             </div>
 
             <div className="simplecrush-showcase">
-
                 <div className="features">
                     <h2>Features</h2>
                     <ul className="feature-list" style={{ paddingBottom: '1rem' }}>
@@ -103,9 +109,10 @@ function SimpleCrushProject() {
                     </div>
                     <div className="settings-themes-showcase">
                         <img src={settingsPic} alt="SimpleCrush Settings" className="setting-pic" />
+                        <img src={presetPic} alt="SimpleCrush Presets" className="preset-pic" />
                         <img src={themePic} alt="SimpleCrush Themes" className="setting-pic" />
                     </div>
-                    <p style={{ textAlign: 'center' }}>I have now finished implementing 7 unique themes that can be selected in the settings menu, as well as a resizing tool.</p>
+                    <p style={{ textAlign: 'center' }}>I have now finished implementing 7 unique themes that can be selected in the settings menu.</p>
                     <div className="theme-grid">
                         {[t1, t2, t3, t4, t5, t6, t7].map((pic, i) => (
                             <img key={i} src={pic} alt={`Theme ${i + 1}`} className="theme-pic" />
@@ -120,7 +127,6 @@ function SimpleCrushProject() {
                         JUCE provides a powerful and flexible platform for audio plugin development, allowing for efficient handling of audio processing and user interface design. The plugin's core functionality is implemented in C++, leveraging JUCE's audio processing capabilities to manipulate the audio signal in real-time.
                         <br></br><br></br>
                         The bit depth and sample rate reduction algorithms are designed to introduce the characteristic distortion associated with bitcrushing, while the mix and filter controls allow users to remove unpleasant high and low frequencies. The development process involved iterating on the audio processing algorithms to achieve the desired sound quality and performance, as well as designing an intuitive user interface for seamless interaction with the plugin.</p>
-
                 </div>
 
                 <div className="project-list">
@@ -130,9 +136,7 @@ function SimpleCrushProject() {
                             <li key={index}>{exp}</li>))}
                     </ul>
                 </div>
-
             </div>
-
         </div>
     );
 }
